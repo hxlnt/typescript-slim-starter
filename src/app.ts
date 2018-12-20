b930c01a70d3a5cda706293d41b0473eee67d5fd
Rachel Weil <raweil@microsoft.com>
2018-12-20 15:19:44 -0600

    Step 4: Add linting (tslint)

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,10 @@
 console.log(`Hello, Typescript!`);
 
 export default class Greeter {
-	byName(name : string) : string {
-		return `Hi, ${name}.`;
+    public byName(name: string): string {
+        return `Hi, ${name}.`;
     }
-    byNameLength(name : string) : string {
+    public byNameLength(name: string): string {
         return `Hi, person with ${name.length} letters in their name.`;
     }
 }
